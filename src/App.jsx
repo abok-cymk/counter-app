bd68e58b7316c67112d7c0ef7422e7efcdc599c2
abok-cymk <allanabok18@gmail.com>
2025-04-30 20:34:27 +0300

    finished the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+import CounterHistory from "./components/CounterHistory";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([0]);
+
+  // Finction to update count and history
+  const updateCount = (newCount) => {
+    setCount(newCount);
+
+    // Update history with the new count
+    setHistory((prevHistory) => {
+      const updatedHistory = [...prevHistory, newCount];
+      return updatedHistory.slice(-5); // Keep only the last 5 values
+    });
+  };
+
+  // Function to restore a count from history
+  const restoreCount = (restoredCount) => {
+    setCount(restoredCount);
+  }
   return (
-    <div>
-      
+    <div className="bg-gray-900 text-white min-h-screen p-4 flex flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">Counter App</h1>
+      <Counter count={count} onCountChange={updateCount}/>
+      <CounterHistory history={history} onRestore={restoreCount}/>
     </div>
   );
 }
